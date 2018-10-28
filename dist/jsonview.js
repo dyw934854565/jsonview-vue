a310ae61228742e438617590cd2cfb728186753d
duanyuwen <duanyuwen@meituan.com>
2018-10-28 17:13:30 +0800

    update

--- dist/jsonview.js
+++ dist/jsonview.js
@@ -385,7 +385,7 @@ module.exports = {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__jsonview_vue_vue_type_template_id_70cfc3cc___ = __webpack_require__(85);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__jsonview_vue_vue_type_template_id_a8b40dd8___ = __webpack_require__(14);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__jsonview_vue_vue_type_script_lang_js___ = __webpack_require__(5);
 /* harmony namespace reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in __WEBPACK_IMPORTED_MODULE_1__jsonview_vue_vue_type_script_lang_js___) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return __WEBPACK_IMPORTED_MODULE_1__jsonview_vue_vue_type_script_lang_js___[key]; }) }(__WEBPACK_IMPORT_KEY__));
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__jsonview_vue_vue_type_style_index_0_lang_css___ = __webpack_require__(43);
@@ -400,8 +400,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 var component = Object(__WEBPACK_IMPORTED_MODULE_3__node_modules_vue_loader_lib_runtime_componentNormalizer_js__["a" /* default */])(
   __WEBPACK_IMPORTED_MODULE_1__jsonview_vue_vue_type_script_lang_js___["default"],
-  __WEBPACK_IMPORTED_MODULE_0__jsonview_vue_vue_type_template_id_70cfc3cc___["a" /* render */],
-  __WEBPACK_IMPORTED_MODULE_0__jsonview_vue_vue_type_template_id_70cfc3cc___["b" /* staticRenderFns */],
+  __WEBPACK_IMPORTED_MODULE_0__jsonview_vue_vue_type_template_id_a8b40dd8___["a" /* render */],
+  __WEBPACK_IMPORTED_MODULE_0__jsonview_vue_vue_type_template_id_a8b40dd8___["b" /* staticRenderFns */],
   false,
   null,
   null,
@@ -413,8 +413,28 @@ component.options.__file = "jsonview.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
 
 /***/ }),
-/* 14 */,
-/* 15 */,
+/* 14 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_jsonview_vue_vue_type_template_id_a8b40dd8___ = __webpack_require__(15);
+/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "a", function() { return __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_jsonview_vue_vue_type_template_id_a8b40dd8___["a"]; });
+/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "b", function() { return __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_jsonview_vue_vue_type_template_id_a8b40dd8___["b"]; });
+
+
+/***/ }),
+/* 15 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return staticRenderFns; });
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{class:['json-view', _vm.length ? 'closeable' : '']},[(_vm.length)?_c('span',{class:['angle', _vm.innerclosed ? 'closed' : ''],on:{"click":_vm.toggleClose}}):_vm._e(),_vm._v(" "),_c('div',{staticClass:"content-wrap"},[_c('p',{staticClass:"first-line"},[(_vm.jsonKey)?_c('span',[_vm._v("\""+_vm._s(_vm.jsonKey)+"\": ")]):_vm._e(),_vm._v(" "),(_vm.length)?_c('span',[_vm._v("\n        "+_vm._s(_vm.prefix)+"\n        "+_vm._s(_vm.innerclosed ? ('...' + _vm.subfix + ' // ' + _vm.length + 'item' + (_vm.length > 1 ? 's' : '')) : '')+"\n      ")]):_vm._e(),_vm._v(" "),(!_vm.length)?_c('span',[_vm._v(_vm._s(_vm.isArray ? '[]' : '{}'))]):_vm._e()]),_vm._v(" "),(!_vm.innerclosed && _vm.length)?_c('div',{staticClass:"json-body"},[_vm._l((_vm.items),function(item,index){return [(item.isJSON)?_c('json-view',{key:index,attrs:{"closed":_vm.closed,"json":item.value,"jsonKey":item.key,"isLast":index === _vm.items.length - 1}}):_c('p',{key:index,staticClass:"json-item"},[_vm._v(_vm._s((_vm.isArray ? '' : '"' + item.key + '": ') + item.value + (index === _vm.items.length - 1 ? '' : ',')))])]}),_vm._v(" "),_c('span',{directives:[{name:"show",rawName:"v-show",value:(!_vm.innerclosed),expression:"!innerclosed"}],staticClass:"body-line"})],2):_vm._e(),_vm._v(" "),(!_vm.innerclosed && _vm.length)?_c('p',{staticClass:"last-line"},[_c('span',[_vm._v(_vm._s(_vm.subfix))])]):_vm._e()])])}
+var staticRenderFns = []
+
+
+
+/***/ }),
 /* 16 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -864,7 +884,7 @@ exports = module.exports = __webpack_require__(45)(false);
 
 
 // module
-exports.push([module.i, "\n.json-view{position:relative;display:block;width:100%;height:100%;overflow:auto;padding-left:20px\n}\n.json-view .json-item{margin:0;padding-left:20px\n}\n.json-view .first-line{padding:0;margin:0\n}\n.json-view .json-body{position:relative;padding:0;margin:0\n}\n.json-view .json-body .body-line{position:absolute;height:100%;width:0;border-left:1px dashed #bbb;top:0;left:2px\n}\n.json-view .last-line{padding:0;margin:0\n}\n.json-view .angle{position:absolute;display:block;cursor:pointer;float:left;width:20px;text-align:center;left:0\n}\n.json-view .angle:after{content:\"\";display:inline-block;width:0;height:0;vertical-align:middle;border-top:4px solid #333;border-left:6px solid transparent;border-right:6px solid transparent\n}\n.json-view .angle.closed:after{border-left:4px solid #333;border-top:6px solid transparent;border-bottom:6px solid transparent\n}", ""]);
+exports.push([module.i, "\n.json-view{position:relative;display:block;width:100%;height:100%;white-space:nowrap;padding-left:20px\n}\n.json-view .json-item{margin:0;padding-left:20px\n}\n.json-view .first-line{padding:0;margin:0\n}\n.json-view .json-body{position:relative;padding:0;margin:0\n}\n.json-view .json-body .body-line{position:absolute;height:100%;width:0;border-left:1px dashed #bbb;top:0;left:2px\n}\n.json-view .last-line{padding:0;margin:0\n}\n.json-view .angle{position:absolute;display:block;cursor:pointer;float:left;width:20px;text-align:center;left:0\n}\n.json-view .angle:after{content:\"\";display:inline-block;width:0;height:0;vertical-align:middle;border-top:4px solid #333;border-left:6px solid transparent;border-right:6px solid transparent\n}\n.json-view .angle.closed:after{border-left:4px solid #333;border-top:6px solid transparent;border-bottom:6px solid transparent\n}", ""]);
 
 // exports
 
@@ -1531,64 +1551,6 @@ function normalizeComponent (
     options: options
   }
 }
-
-
-/***/ }),
-/* 49 */,
-/* 50 */,
-/* 51 */,
-/* 52 */,
-/* 53 */,
-/* 54 */,
-/* 55 */,
-/* 56 */,
-/* 57 */,
-/* 58 */,
-/* 59 */,
-/* 60 */,
-/* 61 */,
-/* 62 */,
-/* 63 */,
-/* 64 */,
-/* 65 */,
-/* 66 */,
-/* 67 */,
-/* 68 */,
-/* 69 */,
-/* 70 */,
-/* 71 */,
-/* 72 */,
-/* 73 */,
-/* 74 */,
-/* 75 */,
-/* 76 */,
-/* 77 */,
-/* 78 */,
-/* 79 */,
-/* 80 */,
-/* 81 */,
-/* 82 */,
-/* 83 */,
-/* 84 */,
-/* 85 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_jsonview_vue_vue_type_template_id_70cfc3cc___ = __webpack_require__(86);
-/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "a", function() { return __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_jsonview_vue_vue_type_template_id_70cfc3cc___["a"]; });
-/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "b", function() { return __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_jsonview_vue_vue_type_template_id_70cfc3cc___["b"]; });
-
-
-/***/ }),
-/* 86 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return render; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return staticRenderFns; });
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{class:['json-view', _vm.length ? 'closeable' : '']},[(_vm.length)?_c('span',{class:['angle', _vm.innerclosed ? 'closed' : ''],on:{"click":_vm.toggleClose}}):_vm._e(),_vm._v(" "),_c('div',{staticClass:"content-wrap"},[_c('p',{staticClass:"first-line"},[(_vm.jsonKey)?_c('span',[_vm._v("\""+_vm._s(_vm.jsonKey)+"\": ")]):_vm._e(),_vm._v(" "),(_vm.length)?_c('span',[_vm._v("\n        "+_vm._s(_vm.prefix)+"\n        "+_vm._s(_vm.innerclosed ? ('...' + _vm.subfix + ' // ' + _vm.length + 'item' + (_vm.length > 1 ? 's' : '')) : '')+"\n      ")]):_vm._e(),_vm._v(" "),(!_vm.length)?_c('span',[_vm._v(_vm._s(_vm.isArray ? '[]' : '{}'))]):_vm._e()]),_vm._v(" "),(!_vm.innerclosed && _vm.length)?_c('div',{staticClass:"json-body"},[_vm._l((_vm.items),function(item,index){return [(item.isJSON)?_c('json-view',{key:index,attrs:{"closed":_vm.closed,"json":item.value,"jsonKey":item.key,"isLast":index === _vm.items.length - 1}}):_c('p',{key:index,staticClass:"json-item"},[_vm._v(_vm._s((_vm.isArray ? '' : '"' + item.key + '": ') + item.value + (index === _vm.items.length - 1 ? '' : ',')))])]}),_vm._v(" "),_c('span',{directives:[{name:"show",rawName:"v-show",value:(!_vm.innerclosed),expression:"!innerclosed"}],staticClass:"body-line"})],2):_vm._e(),_vm._v(" "),(!_vm.innerclosed && _vm.length)?_c('p',{staticClass:"last-line"},[_c('span',[_vm._v(_vm._s(_vm.subfix))])]):_vm._e()])])}
-var staticRenderFns = []
-
 
 
 /***/ })
